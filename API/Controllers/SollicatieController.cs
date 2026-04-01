0a0c00f7d8d8fd9aeedde4bbac01b7f7fed8248e
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-01 14:26:51 +0200

    fix: :bug: Improve formatting and consistency in SollicatieController

--- API/Controllers/SollicatieController.cs
+++ API/Controllers/SollicatieController.cs
@@ -3,16 +3,16 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
-public class SollicatieController: ControllerBase {
-    
+public class SollicatieController : ControllerBase {
     private readonly ISollicitatieService _service;
-    
+
     public SollicatieController(ISollicitatieService sollicitatieService) {
         _service = sollicitatieService;
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> GetAll() {
         var sollicitaties = await _service
@@ -20,20 +20,26 @@ public class SollicatieController: ControllerBase {
 
         return Ok(sollicitaties);
     }
-    
+
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id) {
         var sollicitatie = await _service
             .GetByIdAsync(id);
-        
-        if  (sollicitatie == null) return NotFound();
-        
+        if (sollicitatie == null) return NotFound();
         return Ok(sollicitatie);
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateSollicitatie(CreateSollicitatieDto dto) {
+        if (string.IsNullOrEmpty(dto.Bedrijfsnaam) ||
+            string.IsNullOrEmpty(dto.Status) ||
+            string.IsNullOrEmpty(dto.Locatie)) return BadRequest();
+
         var sollicitatie = await _service.CreateAsync(dto);
-        return Ok(sollicitatie);
+        if (sollicitatie == null) return BadRequest();
+        return CreatedAtAction(
+            nameof(GetById),
+            new{ id = sollicitatie.id }, sollicitatie
+        );
     }
 }
