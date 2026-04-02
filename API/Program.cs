88485cb04a36789dc239c318dc8dcd83dcf0c45a
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-02 19:51:55 +0200

    feat: add PostgreSQL support for Railway

--- API/Program.cs
+++ API/Program.cs
@@ -4,16 +4,41 @@ using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
 builder.Services.AddScoped<ISollicitatieService, SollicitatieService>();
-builder.Services.AddDbContext<SollicitatieContext>(options =>
+
+/*
+ builder.Services.AddDbContext<SollicitatieContext>(options =>
     options.UseSqlite("Data Source=sollicitaties.db"));
+builder.Services.AddOpenApi();
+*/
+
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+builder.Services.AddDbContext<SollicitatieContext>(options => {
+    if (!string.IsNullOrWhiteSpace(databaseUrl)) {
+        var uri = new Uri(databaseUrl);
+        var userInfo = uri.UserInfo.Split(':');
+        var connectionString =
+            $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+        options.UseNpgsql(connectionString);
+    }
+    else {
+        options.UseSqlite("Data Source=sollicitaties.db");
+    }
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SollicitatieContext>();
+    db.Database.Migrate();
+}
+
+
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
-app.MapControllers();  // ✅ dit miste!
+app.MapControllers(); 
 app.Run();
